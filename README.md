06b1a5680bc622ec394dbed81b8f741864cc3f81
Rama Krishna Chavali <chavali.r@northeastern.edu>
2026-01-01 16:19:00 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Terraform
+This Repo sets a foundation to understanding Terraform (IaC)
---
+++ service_account.tf
@@ -0,0 +1,4 @@
+resource "google_service_account" "terraform-practice-service-account" {
+  account_id   = "terraform-practice"
+  display_name = "Service account to practice terraform provisioning"
+}
