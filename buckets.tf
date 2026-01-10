50911391bbdc8ecd66dfca5e788f840f3f4c7720
Rama Krishna Chavali <chavali.r@northeastern.edu>
2026-01-09 21:09:07 -0500

    bucket added

--- buckets.tf
+++ buckets.tf
@@ -2,3 +2,9 @@ resource "google_storage_bucket" "terraform_state"{
   name ="terraform_state_remote-rk"
   location = var.region
 }
+
+
+resource "google_storage_bucket" "test_bucket"{
+  name ="test"
+  location = var.region
+}
